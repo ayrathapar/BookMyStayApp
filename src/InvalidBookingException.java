d2c7c8f791945d02167053f174df3cab984dab16
ayrathapar <thaparayra07@gmail.com>
2026-03-12 09:49:37 +0530

    UC9: Error handling and validation with custom exception

---
+++ src/InvalidBookingException.java
@@ -0,0 +1,11 @@
+/**
+ * CLASS - InvalidBookingException
+ * Use Case 9: Error Handling & Validation
+ */
+
+public class InvalidBookingException extends Exception {
+
+    public InvalidBookingException(String message) {
+        super(message);
+    }
+}
