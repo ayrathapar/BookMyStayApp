d0936a5720b0b66c4a14fec042102ae15e043bae
ayrathapar <thaparayra07@gmail.com>
2026-03-12 08:49:20 +0530

    UC1: Application entry and welcome message

---
+++ src/UseCase1HotelBookingApp.java
@@ -0,0 +1,23 @@
+/**
+ * Book My Stay - Hotel Booking System
+ *
+ * UC1: Application Entry and Welcome Message
+ *
+ * @author Ayra
+ * @version 1.0
+ */
+
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("===================================");
+        System.out.println("        BOOK MY STAY APP           ");
+        System.out.println("      Hotel Booking System         ");
+        System.out.println("           Version 1.0             ");
+        System.out.println("===================================");
+
+        System.out.println("Welcome to Book My Stay!");
+        System.out.println("Your smart hotel booking solution.");
+    }
+}
